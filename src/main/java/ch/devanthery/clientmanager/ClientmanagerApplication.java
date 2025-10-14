bae5d8865eaac7f9b604c711544877b956145fc5
Devanthery Noa <Noa.Devanthery@he-arc.ch>
2025-10-14 11:39:33 +0200

    initial commit

---
+++ src/main/java/ch/devanthery/clientmanager/ClientmanagerApplication.java
@@ -0,0 +1,13 @@
+package ch.devanthery.clientmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/devanthery/clientmanager/ClientmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.devanthery.clientmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
